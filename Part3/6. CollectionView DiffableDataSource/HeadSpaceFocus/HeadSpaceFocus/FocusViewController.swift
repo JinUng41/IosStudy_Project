704604827d8226df6de24101e71f438bdff37ced
JinUng <ingjwjw@naver.com>
2022-08-01 15:46:01 +0900

    upload HeadSpaceFocus

---
+++ Part3/6. CollectionView DiffableDataSource/HeadSpaceFocus/HeadSpaceFocus/FocusViewController.swift
@@ -0,0 +1,95 @@
+//
+//  FocusViewController.swift
+//  HeadSpaceFocus
+//
+//  Created by 김진웅 on 2022/08/01.
+//
+
+import UIKit
+
+class FocusViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    var curated: Bool = false
+    
+    var items: [Focus] = Focus.list
+    
+    enum Section {
+        case main
+    }
+    typealias Item = Focus
+    
+    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        refreshButton.layer.cornerRadius = 10
+
+        // Presentation, Data, Layout
+        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FocusCell", for: indexPath) as? FocusCell else {
+                return nil
+            }
+            
+            cell.configure(item)
+            return cell
+        })
+        
+        // Snapshot
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        datasource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
+        
+        updateButtonTitle()
+    }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        // layout, section, group, item
+        // group 안에는 1개의 item 만 있음
+        
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        // .estimated(50) 기본적으로 50으로 설정을 하나 가변적임
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(50))
+        
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        // 섹션의 상하좌우 여백
+        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
+        // 그룹과 그룹 간의 여백
+        section.interGroupSpacing = 10
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    // 버튼 이름 바꾸는 코드
+    func updateButtonTitle() {
+        let title = curated ? "See All" : "See Recommendation"
+        refreshButton.setTitle(title, for: .normal)
+    }
+    
+    @IBAction func refreshButtonTapped(_ sender: Any) {
+        // curated는 bool 타입으로 bool 타입을 가지는 경우 toggle을 통해 값을 바꿀수 있다. true <-> false
+        curated.toggle()
+        
+        self.items = curated ? Focus.recommendations : Focus.list
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        datasource.apply(snapshot)
+        
+        updateButtonTitle()
+    }
+}
